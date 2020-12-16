288d0066745957a8c55dc5246e4e30b75f9b8f4e
rayetzki <rayetzkiillya@gmail.com>
2020-12-16 19:34:39 +0200

    Error if geolocation not found test

--- src/hooks/__tests__/useCurrentWeather.test.js
+++ src/hooks/__tests__/useCurrentWeather.test.js
@@ -23,6 +23,32 @@ it("Returns weather by coordinates", async () => {
     lon: coords.longitude,
   });
   await act(async () => {});
-  expect(result.current.error).toBeNull();
-  expect(result.current.weather).toStrictEqual(apiResponse);
+  const { error, weather } = result.current;
+  expect(error).toBeNull();
+  expect(weather).toStrictEqual(apiResponse);
+});
+
+it("Returns an error if the weather for the location was not found", async () => {
+  mockGetWeatherData.mockRejectedValueOnce(
+    "Нету данных по твоему местоположению"
+  );
+  const coords = { latitude: 51.1, longitude: 45.3 };
+  window.navigator.geolocation = {
+    getCurrentPosition: jest
+      .fn()
+      .mockImplementationOnce((success) => success({ coords })),
+  };
+  const { result } = renderHook(useCurrentWeather);
+  await act(async () => {});
+  const { error, weather } = result.current;
+  expect(error).toBe("Нету данных по твоему местоположению");
+  expect(weather).toBeNull();
+});
+
+it("Returns an appropriate error if geolocation API is not found", () => {
+  delete window.navigator.geolocation;
+  const { result } = renderHook(useCurrentWeather);
+  const { error, weather } = result.current;
+  expect(error).toBe("Геолокация недоступная. Попробуйте ручной режим");
+  expect(weather).toBeNull();
 });
